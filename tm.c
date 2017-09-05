5f976cca294d170972846d315d334148c5f471b3
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2017-09-05 16:21:05 +0530

    Create tm.c

---
+++ tm.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter the value");
+scanf("%d",&i):
+if(i<0)
+{
+printf("the value is positive");
+}
+else
+{
+printf("the value is negative");
+}
+if(i==0)
+{
+printf("the value is zero");
+}
+}
+
