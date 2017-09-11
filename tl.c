51fbd63862ce713ea9d2519d889571d87976e964
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2017-09-11 19:28:57 +0530

    Create tl.c

---
+++ tl.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+prinf("enter the number");
+scanf("%d",&number);
+if(number%2==0)
+printf("%d" is even number);
+else
+printf("%d" is odd number);
+else if(number==0)
+printf("%d"the number is zero);
+}
