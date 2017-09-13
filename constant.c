d2209c382a3aaa7f14aa910bdf616815d7a00230
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2017-09-13 19:41:10 +0530

    Rename vowel.c to constant.c

---
+++ constant.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int ch;
+printf("enter the character");
+scanf("%d",&ch);
+if(ch=='a'!!ch=='e'!!ch=='i'!!ch=='o'!!ch=='u'!!ch=='A'!!ch=='E'!!ch=='I'!!ch=='O'!!ch=='U')
+{
+printf("\n the number is vowel");
+}
+else
+{
+printf("\n the number is constant");
+}
+}
