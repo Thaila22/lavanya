14369e9d3d080ab1304637a90b62c9d81a76589f
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-19 15:58:42 +0530

    Create 2 intervals.c

---
+++ 2 intervals.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,m,n;
+printf("enter the value of m and n");
+scanf("%d",&m,&n);
+for(i=m;i<=n;i++)
+{
+if(i%2==1)
+{
+printf("%d", the odd numbers are");
+}
+}
