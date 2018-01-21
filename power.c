843a80fb0057654e3b8b9f6dd58206bfa3ab0cf1
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-21 04:33:28 +0000

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n=2;
+printf("enter the value of i);
+scanf("%d",&i);
+for(i=0;i<=n;i++);
+{
+if(i==n)
+{
+i^2*i^2*i^2;
+}
+return 0;
+}
+}
