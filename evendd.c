f1c64f21ae93c6e1609b39ea7fdc264130a5a1a3
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-21 04:18:29 +0000

    Create evendd.c

---
+++ evendd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter the value of n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d",even number);
+}
+else
+{
+printf("%d",odd number);
+}
+return 0;
+}
