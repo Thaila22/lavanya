8779477ece8a94e99b19d5ff1dbba62ade01d736
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-21 05:30:09 +0000

    Create even interval.c

---
+++ even interval.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,m,n;
+printf("enter the range of m and n);
+scanf("%d","%d",&m,&n);
+for(i=m;i<=n;i++)
+{
+if(n%2==0)
+{
+printf("%d",even numbers are);
+}
+return 0;
+}
+}
