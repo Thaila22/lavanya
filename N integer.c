dfec71d2c8f02850126335ea51f320df0ff346af
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-23 15:54:07 +0530

    Create N integer.c

---
+++ N integer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int l,i,a[100],sum=0,k;
+printf("enter the size of an array");
+scanf("%d",&l);
+printf("enter the array");]
+for(i=0;i<=l;i++)
+{
+scanf("%d",a[i]);
+}
+printf("enter the no.of sum");
+scanf("%d",&m);
+for(i=0;i<=k;i++)
+{
+sum=sum+a[i];
+}
+printf("sum is=%d",sum);
+
+
+
