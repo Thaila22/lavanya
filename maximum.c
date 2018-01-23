4a5998e6037f50215af711514f29f8d6c0efa60b
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-23 16:28:05 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,temp,k[40];
+printf("enter the number \n");
+scanf("%d",&n);
+printf("enter the array of n number");
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<n;i++);
+{
+for(j=j+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+printf("maximum no is a[n-1] /d/t");
+}
+}
+}
+
+
