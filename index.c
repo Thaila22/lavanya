c0ffd6b20eb01c4b0a92f64465aeae4b928bb251
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-24 18:10:42 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,17 @@
+#include<stdi.h> 
+void main()
+{
+int i,arr[100],num;
+printf("enter the number");
+scanf("%d",&num);
+printf("enter the element");
+}
+for(i=0;i<num;i++)
+{
+scanf("%d",a[i]);
+}
+for(j=0;i<num;i++)
+{
+printf("arr[i]=%d,i,arr(n));
+}
+return 0;
