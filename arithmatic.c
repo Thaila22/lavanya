22401b11ef9c3d7e20f53c7d1b9b18f4b093ca15
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-25 12:41:25 +0000

    Create arithmatic.c

---
+++ arithmatic.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+int N,A,D;
+int sum=0;
+scanf("%d\t%d\t%d",&N,&A,&D);
+if(A<=100000&&D<=100000)
+{
+for(1=A;i<=N;i=i+D)
+{
+sum=sum+i;
+N--;
+}
+printf("%d",sum);
+}
+}
+
