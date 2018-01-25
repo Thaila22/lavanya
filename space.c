837d604f5dc637dbf3b33b6d6584f5d66c6c120f
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-01-25 12:19:55 +0000

    Update space.c

--- space.c
+++ space.c
@@ -5,4 +5,12 @@ char a[1000];
 int i,count=0;
 printf("enter the string\n");
 scanf("%[^n]s",&a)
-for(i=0;a[i]
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]='')
+  {
+    count++;
+  }
+  printf("%d",count);
+}
+  
