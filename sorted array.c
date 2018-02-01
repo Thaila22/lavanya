ffd562ee971766c5c3f9f77d7886190bf3a79110
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-02-01 16:00:01 +0530

    Create sorted array.c

---
+++ sorted array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,j,temp,n,a[10];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
