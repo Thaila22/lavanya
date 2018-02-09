121b595f7fb02e679fa468cb194a6f18ef4921c0
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-02-09 09:32:23 +0000

    Create near multiple10 70.c

---
+++ beginner/set7/near multiple10 70.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main9void)
+{
+int a,n;
+scanf("%d",&a);
+if(n<=1000)
+{
+ for(i=0;i<n;i++)
+ {
+ a=a*2;
+ }
+ if(a==n)
+ {
+ printf("%d",n*2);
+ break;
+ }
+ return 0;
+ }
+ 
