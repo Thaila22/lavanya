7e931b3cb368e35e8e203ff36141986303f72eb7
Thaila22 <30829603+Thaila22@users.noreply.github.com>
2018-02-26 18:05:39 +0530

    Update 73.c

--- beginner/set8/73.c
+++ beginner/set8/73.c
@@ -2,4 +2,21 @@
 int main(void)
 {
 int i,count=0,a,b,n;
-
+  scanf("%d",&n);
+  scanf("%d\t%d",&a,&b);
+  for(i=a;i<=b;i++)
+  {
+    if(i==n)
+    {
+      count++;
+    }
+    if(count>0)
+    {
+      printf("yes");
+    }
+    else
+    {
+      printf("no");
+    }
+    return 0;
+  }
